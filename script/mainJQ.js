2b2d88340c8afa52bdd400e96f4f0098642e44b5
Christie <r_christie3@tss-hr420-dm30.fc.ca>
2017-02-03 15:54:37 -0500

    new branch with jq

--- script/mainJQ.js
+++ script/mainJQ.js
@@ -1,45 +1,14 @@
 (function(){
-	console.log("loading main jq");
-	var pokeImages = document.querySelectorAll("nav li"),
-	critterHeader = document.querySelector(".click-header"),
-	criterImage= document.querySelector(".pokemon-large"),
-	critterDesc=document.querySelector(".content-section p"),
-	habHeader= document.querySelector(".habitat-header"),
-	critterHab=document.querySelector(".habitat"),
-	httpRequest;
+	$('nav li').on('click', function(){
+		$.getJSON('includes/getPokemon.php',{ critter : this.id}, function(data){
+			console.log(data);
 
-	function makeRequest(){
-		httpRequest = new XMLHttpRequest();
-
-		if(!httpRequest){
-			console.log('please update browser');
-			return false;
-		}
-		httpRequest.onreadystatechange = showPokemonInfo;
-		httpRequest.open('GET', 'includes/getPokemon.php' + '?critter=' + this.id);
-		httpRequest.send();
-	}
-
-	function showPokemonInfo(){
-		if(httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200){
-			var pokeData = JSON.parse(httpRequest.responseText);
-
-			critterHeader.firstChild.nodeValue = pokeData.pokeName;
-
-			[].forEach.call(document.querySelectorAll('.hidden'), function(item){
-				item.classList.remove('.hidden');
-			});
-			console.log("line 32");
-			criterImage.src = "images/" + pokeData.pokeImage + ".png";
-			critterDesc.firstChild.nodeValue = pokeData.pokeDesc;
-			habHeader.firstChild.nodeValue = pokeData.pokeName + " lives here!";
-			critterHab.src = "images/" + pokeData.bgImage + ".jpg";
-			console.log(pokeData.pokeImages);
-		}
-	}
-			
-	[].forEach.call(pokeImages, function(img) {
-		img.addEventListener("click", makeRequest, false);
+			$('.click-header').text(data.pokeName);
+			$('.hidden').removeClass('hidden');
+			$('.pokemon-large').attr('src','images/' + data.pokeImage + '.png');
+			$('.content-section p').text(data.pokeDesc);
+			$('.habitat-header').text(data.pokeName + "lives here!");
+			$('.habitat').attr('src', 'images/' + data.bgImage + '.jpg');
+		});
 	});
-
 })();
